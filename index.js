5e837f00a7b584047d54fef146064a8979c0444c
OP-Patel <ompately5t6@gmail.com>
2024-03-30 22:19:57 -0400

    finish inital header

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import express from "express";
-import axios from "axios";
-import bodyParser from "body-parser";
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express(); 
 const port = 3000; 
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
